09d475e3a2b135b3e70b5fd4a2f8a28fb81a542c
Popugai28 <pavvyb@icloud.com>
2021-05-22 13:43:35 +0300

    Add responsiveness
    Add breakpoints using mediaqueries implemented in a separate 'responsive.css' file
    
    Move all styles into separate 'styles.css' file for more convenience

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './styles.css'
+import backgroundTop from './images/background/topBackgroundBig.png'
+import backgroundBottom from './images/background/bottomBackgroundBig.png'
 
 function App () {
   return (
     <div className="App">
-      <header className="App-header">
+      {
+        <div
+          className="backgroundImage"
+          style={{ backgroundImage: `url(${backgroundTop}), url(${backgroundBottom})` }}
+        >
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +27,7 @@ function App () {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './styles.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './fonts/Ubuntu-Regular.ttf'
+import './responsive.css'
 
 ReactDOM.render(
   <React.StrictMode>
